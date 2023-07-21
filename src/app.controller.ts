e218a4a8822b29273655fb98a3abb3da46e86462
Heemale <1916737051@qq.com>
2023-07-21 15:15:45 +0800

    Add get_market_price api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,16 @@
-import {Controller} from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {ApiOperation, ApiTags} from "@nestjs/swagger";
+import {GetAmountsOutDto} from "./web3/dto/get-amounts-out.dto";
+import {get_token_price} from "./web3";
 
+@ApiTags('查询')
 @Controller()
 export class AppController {
+
+    @ApiOperation({summary: '获取市值'})
+    @Post('/market/price')
+    async approve_admin_batch(@Body() getAmountsOutDto: GetAmountsOutDto): Promise<string> {
+        return await get_token_price(getAmountsOutDto);
+    }
 
 }
