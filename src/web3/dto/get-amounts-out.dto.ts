1560480ce007026f6a78a63edb6b68bbc573cc7a
Heemale <1916737051@qq.com>
2023-07-21 15:02:51 +0800

    Modify description text

--- src/web3/dto/get-amounts-out.dto.ts
+++ src/web3/dto/get-amounts-out.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
+import {ApiProperty} from '@nestjs/swagger';
 
 export class GetAmountsOutDto {
 
@@ -8,13 +8,22 @@ export class GetAmountsOutDto {
         this.contract_address = contract_address;
     }
 
-    @ApiProperty({ description: '输入代币数量' })
+    @ApiProperty({description: '输入代币数量'})
     amount_in: number;
 
-    @ApiProperty({ description: '授权额度' })
+    @ApiProperty({
+        description: '兑换路径',
+        example: [
+            "0x397b5b9066d72ed34e5b8f4c5336a998cb2b61d1",
+            "0x546028cac67fb85b00260c004695d975a23b0515"
+        ]
+    })
     tokens: string[];
 
-    @ApiProperty({ description: 'contract_address' })
+    @ApiProperty({
+        description: 'contract_address',
+        example: "0xCACd778509Eb52E894172f69D0BB6EF8eeb32F48"
+    })
     contract_address: string;
 
 }
